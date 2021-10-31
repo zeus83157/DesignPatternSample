fc18229927c8ec6eb93a087cb1a6665fc9f0da2b
Kernel Yang <zeus83157@gmail.com>
2021-11-01 00:04:14 +0800

    Add Singleton

---
+++ C001.Singleton/Component.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C001.Singleton
+{
+    sealed class Component
+    {
+        private static object _syncRoot = new object();
+        private int i = 0;
+        private static Component _componentObj;
+        private Component()
+        { }
+        public static Component ComponentObj
+        {
+            get
+            {
+                if (_componentObj == null)
+                {
+                    lock (_syncRoot)
+                    {
+                        if (_componentObj == null)
+                        {
+                            _componentObj = GetInstance();
+                        }
+                    }
+                }
+                return _componentObj;
+            }
+        }
+        private static Component GetInstance()
+        {
+            return new Component();
+        }
+        public void Show()
+        {
+            Console.WriteLine(i);
+            i += 1;
+        }
+        public void Clear()
+        {
+            _componentObj = null;
+        }
+
+    }
+}
---
+++ C001.Singleton/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C001.Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Component.ComponentObj.Show();
+            Component.ComponentObj.Show();
+            Component.ComponentObj.Clear();
+            Component.ComponentObj.Show();
+            Component.ComponentObj.Show();
+
+        }
+    }
+}
